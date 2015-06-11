c952a49c3afd2023d7e765ed12542425b1015eb7
Yoshinari Nomura <nom@nomlab.org>
2015-06-11 23:44:35 +0900

    New controllers: welcome and sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  skip_before_filter :authenticate
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_path, :notice => "User logout."
+  end
+
+  def create
+    auth = request.env["omniauth.auth"]
+
+    unless auth.credentials.active_member?
+      render text: "Unauthorized", status: 401
+      return false
+    end
+
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
+           User.create_with_omniauth(auth)
+
+    session[:user_id] = user.id
+    session[:access_token] = auth["credentials"]["token"]
+    redirect_to '/', :notice => "User #{user.name} signed in."
+  end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  skip_before_filter :authenticate
+
+  def index
+  end
+end

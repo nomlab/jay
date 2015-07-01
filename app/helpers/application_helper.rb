dbd98c9f710f0c999a716e6240d9b3b7af9eed99
Yoshinari Nomura <nom@nomlab.org>
2015-07-01 19:36:14 +0900

    Add new helper ``new_content''
    quick alias for link_to plus-icon, path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def glyphicon(name)
     raw %(<span class="glyphicon glyphicon-#{name}" aria-hidden="true"></span>)
   end
+
+  def new_content(path)
+    link_to glyphicon("plus"), path,  type: "button", class: "btn btn-default"
+  end
 end

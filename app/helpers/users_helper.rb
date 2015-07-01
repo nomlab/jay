ea54ca0bd1c6ee30cac2f0012e3de389e7cdef02
Yoshinari Nomura <nom@nomlab.org>
2015-07-01 19:37:07 +0900

    Add helper user_avatar_icon_tag
    create image_tag with user's avatar url

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def user_avatar_icon_tag(user, size = 32)
+    if user and avatar_url = user.avatar_url
+      sep  = (/\?/ =~ avatar_url) ? "&" : "?"
+      img  = "#{avatar_url}#{sep}s=#{size}"
+      name = user.screen_name
+      image_tag(img, alt: "@#{name}", class: "avatar")
+    end
+  end
 end

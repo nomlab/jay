e640b6cc6c3f0afa50f60ed8eb7e91ad44e860e8
yuko <ike534xx@yahoo.co.jp>
2016-03-15 10:53:46 +0900

    Use bootstrap flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,24 @@ module ApplicationHelper
   def tag_label(tag)
     raw %(<span class="label label-primary tag-label"><span class="glyphicon glyphicon-tag"></span> #{tag.name}</span>)
   end
+
+  def bootstrap_class_for(flash_type)
+    { success: "alert-success", error: "alert-danger",
+      alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |flash_type, message|
+      concat(
+        content_tag(:div, message, class: "alert alert-dismissable #{bootstrap_class_for(flash_type)} fade in") do
+          concat(
+            content_tag(:button, class: "close", data: { dismiss: "alert" }) do
+              concat content_tag(:span, "&times;".html_safe)
+            end
+          )
+          concat message
+        end
+      )
+    end
+  end
 end

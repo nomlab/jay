53dc36039d92888327423c3e0814dd0aa9abeeb4
mukohara <mukohara2021@s.okayama-u.ac.jp>
2021-07-06 15:00:30 +0900

    Modify markdown_to_plaintext_test.rb

--- test/markdown/markdown_to_plaintext_test.rb
+++ test/markdown/markdown_to_plaintext_test.rb
@@ -2,32 +2,24 @@ require 'test_helper'
 require_relative '../../lib/markdown_converter.rb'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "sample" do
-  #   assert true
-  # end
-  # プレーンテキストのインデントは，半角スペース4つ
 
-  test "space of unordered list" do
+  #行頭に１つのスペースを含む項番なしリストのネストの入力が，ネストではない項番なしリストの出力になる問題を検証するテスト
+  test "should get nesting of unordered list" do
     assert JayFlavoredMarkdownToPlainTextConverter.new("* aaa\n * aaa").content == "* aaa\n    * aaa"
   end
 
-  test "header2" do
+  #文字列の下行の '-' の入力が，2段階見出しの出力になる問題を検証するテスト
+  test "should get '-' below 'aaa'" do
     assert JayFlavoredMarkdownToPlainTextConverter.new("aaa\n-").content == "aaa\n-"
   end
 
-  test "folding in writing" do
-    assert JayFlavoredMarkdownToPlainTextConverter.new("").content == ""
-  end
-
-  test "unordered list after ordered list" do
+  #項番付きリストの下行の項番なしリストの入力が，項番付きリストの出力になる問題を検証するテスト
+  test "should get unordered list below ordered list" do
     assert JayFlavoredMarkdownToPlainTextConverter.new("+ aaa\n* aaa").content == "(1) aaa\n* aaa"
   end
 
-  test "ordered list after unordered list" do
+  #項番なしリストの下行の項番付きリストの入力が，項番なしリストの出力になる問題を検証するテスト
+  test "should get ordered list below unordered list" do
     assert JayFlavoredMarkdownToPlainTextConverter.new("* aaa\n+ aaa").content == "* aaa\n(1) aaa"
-  end
-
-  test "secondary unordered list after ordered list" do # ビュレットが食い込む問題は，プレーンテキストレベルではわからない
-    assert JayFlavoredMarkdownToPlainTextConverter.new("+ aaa\n  * aaa").content == "(1) aaa\n    * aaa"
   end
 end

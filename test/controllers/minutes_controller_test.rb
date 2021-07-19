6ed7de9c69c6420cd7bc76b5f590bc16003198e2
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-07-19 11:51:27 +0900

    Fix tests

--- test/controllers/minutes_controller_test.rb
+++ test/controllers/minutes_controller_test.rb
@@ -1,49 +1,71 @@
-require 'test_helper'
+require "test_helper"
 
-class MinutesControllerTest < ActionController::TestCase
+class MinutesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
     @minute = minutes(:one)
+    OmniAuth.config.test_mode = true
   end
 
   test "should get index" do
-    get :index
+    log_in_as(@user)
+    get minutes_url
     assert_response :success
-    assert_not_nil assigns(:minutes)
   end
 
   test "should get new" do
-    get :new
+    log_in_as(@user)
+    get new_minute_url
     assert_response :success
   end
 
   test "should create minute" do
-    assert_difference('Minute.count') do
-      post :create, minute: { author_id: @minute.author_id, content: @minute.content, dtend: @minute.dtend, dtstart: @minute.dtstart, location: @minute.location, title: @minute.title }
-    end
-
-    assert_redirected_to minute_path(assigns(:minute))
+    log_in_as(@user)
+    post minutes_url, params: {
+      minute: {
+        id: @minute.id,
+        title: @minute.title,
+        dtstart: @minute.dtstart,
+        dtend: @minute.dtend,
+        location: @minute.location,
+        author_id: @minute.author_id,
+        content: @minute.content
+      }
+    }
+    assert_redirected_to minute_url(@minute.id + 1)
   end
 
   test "should show minute" do
-    get :show, id: @minute
+    log_in_as(@user)
+    get minute_url(@minute)
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @minute
+    log_in_as(@user)
+    get edit_minute_url(@minute)
     assert_response :success
   end
 
   test "should update minute" do
-    patch :update, id: @minute, minute: { author_id: @minute.author_id, content: @minute.content, dtend: @minute.dtend, dtstart: @minute.dtstart, location: @minute.location, title: @minute.title }
-    assert_redirected_to minute_path(assigns(:minute))
+    log_in_as(@user)
+    patch minute_url(@minute), params: {
+      minute: {
+        title: @minute.title,
+        dtstart: @minute.dtstart,
+        dtend: @minute.dtend,
+        location: @minute.location,
+        author_id: @minute.author_id,
+        content: @minute.content
+      }
+    }
+    assert_redirected_to minute_url(@minute)
   end
 
-  test "should destroy minute" do
-    assert_difference('Minute.count', -1) do
-      delete :destroy, id: @minute
-    end
-
-    assert_redirected_to minutes_path
+  test "should delete minute" do
+    log_in_as(@user)
+    delete minute_url(@minute)
+    assert_redirected_to minutes_url
   end
+
 end

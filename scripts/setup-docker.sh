65a58c47ba5dd10ac337a6df375255139767caee
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2021-08-31 10:58:09 +0900

    Allow setup-docker.sh to specify user name

--- scripts/setup-docker.sh
+++ scripts/setup-docker.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-cd $(dirname $0)
-cd ../
+# This scripts is for setting up jay image
+# * Create jay image
+# * Create master.key
+# * Migrate database for production
 
-echo "Create jay image"
-UID_SH=$(id -u)
-docker build -t jay -f ./Dockerfile_production --build-arg UID=$UID_SH .
+function main() {
+    cd $(dirname $0)
+    cd ../
 
-echo "Setup secret key and database"
-./scripts/launch-docker.sh ./scripts/docker/docker-setup.sh
-./scripts/launch-docker.sh stop
+    if [ "$1" = "" ]; then
+        usage
+        exit 1
+    fi
 
-echo "Done."
+    if ! (user_exists $1); then
+        echo "Specified user does not exist"
+        exit 1
+    fi
+
+    echo "Create jay image"
+    UID_SH=$(id -u $1)
+    docker build -t jay -f ./Dockerfile_production --build-arg UID=$UID_SH .
+
+    echo "Setup secret key and database"
+    ./scripts/launch-docker.sh ./scripts/docker/docker-setup.sh
+    ./scripts/launch-docker.sh stop
+
+    echo "Done."
+}
+
+function usage() {
+    cat <<_EOT_
+setup-docker.sh <UserName>
+<UserName> is the name of user that runs jay container.
+_EOT_
+}
+
+function user_exists() {
+    if id -u $1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+main $1
